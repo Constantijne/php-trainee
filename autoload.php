c66858b01545538b383bbb002d4f6789da9fd253
Constantijne PC <constantijne@gmail.com>
2022-02-22 00:42:44 +0200

    Done autoloader

--- autoload.php
+++ autoload.php
@@ -1,6 +1,10 @@
 <?php
 
-require 'application/core/controller.php';
-require 'application/core/route.php';
-require 'application/core/model.php';
-require 'application/core/view.php';
+spl_autoload_extensions(".php");
+
+spl_autoload_register(function ($class_name) {
+    include 'application/core/view.php';
+    include 'application/core/controller.php';
+    include 'application/core/model.php';
+    include 'application/core/route.php';
+});
